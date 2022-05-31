e7dae8287fe4a20f05a9352390645293a03b8c13
v <ardban@outlook.com>
2022-05-31 20:01:30 +0800

    Remove one for loop

--- algebra/sieve-of-eratosthenes/print_some_primes.c
+++ algebra/sieve-of-eratosthenes/print_some_primes.c
@@ -9,29 +9,27 @@
 
 int print_prime_until_n (int n)
 {
-    int *is_prime = NULL;
+    int *is_not_prime = NULL;
     int i = 0;
     long long j = 0;
     int is_prime_counter = 0;
 
-    is_prime = malloc ((n+1) * sizeof(int));
-    if (is_prime == NULL) return RET_STATUS_FAILURE;
+    is_not_prime = malloc ((n+1) * sizeof(int));
+    if (is_not_prime == NULL) return RET_STATUS_FAILURE;
 
-    is_prime[0] = is_prime[1] = FALSE;
-    for (i = 2; i <= n; i++) {
-        is_prime[i] = TRUE;
-    }
+    is_not_prime[0] = is_not_prime[1] = TRUE;
+
 
     for (i = 2; (long long) i*i <= n; i++) {
-        if (is_prime[i]) {
+        if (!is_not_prime[i]) {
             for (j = i*i; j <= n; j+=i) {
-                is_prime[j] = FALSE;
+                is_not_prime[j] = TRUE;
             }
         }
     }
 
     for (i = 2; i <= n; i++) {
-        if (is_prime[i] == TRUE) {
+        if (is_not_prime[i] == FALSE) {
             is_prime_counter++;
             if (is_prime_counter%100 == 1) {
                 printf ("%d\n",i);
